fec6a40a39b8da2be4d39b3f713399c5eb2fc64c
camiqf1 <156013609+camiqf1@users.noreply.github.com>
2025-06-18 18:38:47 -0600

    4.3 - Create a New Page

--- src/main/frontend/src/main.jsx
+++ src/main/frontend/src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import Page2 from "./Page2.jsx";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App />} />
+                <Route path="/page2" element={<Page2 />} />
+                {/* You can add more pages by adding more Route elements here! */}
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>,
 )
